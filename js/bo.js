bcherny/bo

commit 20d07fdc6590ade13bb4a4fe557b5b1b71966355

hide panes initially to prevent all panes showing for a split second onLoad when the DOM is heavy

js/bo.js
@@ -32,7 +32,14 @@ Bo = (function(_super) {
     panes = document.querySelectorAll('[' + this.options.paneAttribute + ']');
     _.each(panes, this.registerPane);
     this.hideAll();
+    this.displayBlock();
     return this.show(_.one(this.panes));
+  };
+
+  Bo.prototype.displayBlock = function() {
+    return this.iterate(function(pane) {
+      return pane.style.display = 'block';
+    });
   };
 
   Bo.prototype.registerPane = function(element) {

dist/bo.css
@@ -1,6 +1,34 @@
-*{border:0;margin:0;padding:0;text-rendering:optimizelegibility;user-select:none;-webkit-tap-highlight-color:rgba(0,0,0,0);-webkit-touch-callout:none;-webkit-user-select:none}
-html,body{height:100%;width:100%}
-[data-bo-pane]{bottom:0;left:0;margin:auto;position:absolute;right:0;top:0;-webkit-transform:translate3d(0,0,0);}
-[data-bo-pane].left{-webkit-transform:translate3d(-100%,0,0)}
-[data-bo-pane].right{-webkit-transform:translate3d(100%,0,0)}
-[data-bo-pane].animate{-webkit-transition:-webkit-transform .2s linear}
+* {
+  border: 0;
+  margin: 0;
+  padding: 0;
+  text-rendering: optimizelegibility;
+  user-select: none;
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+}
+html,
+body {
+  height: 100%;
+  width: 100%;
+}
+[data-bo-pane] {
+  bottom: 0;
+  display: none;
+  left: 0;
+  margin: auto;
+  position: absolute;
+  right: 0;
+  top: 0;
+  -webkit-transform: translate3d(0, 0, 0);
+}
+[data-bo-pane].left {
+  -webkit-transform: translate3d(-100%, 0, 0);
+}
+[data-bo-pane].right {
+  -webkit-transform: translate3d(100%, 0, 0);
+}
+[data-bo-pane].animate {
+  -webkit-transition: -webkit-transform 0.2s linear;
+}

stylus/bo.styl
@@ -18,6 +18,7 @@ body
 
 [data-bo-pane]
 	bottom 0
+	display none
 	left 0
 	margin auto
 	position absolute

commit 2534b294826ecbab3d3d2e3cb04fb554a677426f

fixed bug introduced in 20d07fdc6590ade13bb4a4fe557b5b1b71966355

js/bo.js
@@ -38,7 +38,7 @@ Bo = (function(_super) {
 
   Bo.prototype.displayBlock = function() {
     return this.iterate(function(pane) {
-      return pane.style.display = 'block';
+      return pane.element.style.display = 'block';
     });
   };
 